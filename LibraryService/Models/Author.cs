dd461cf933615b7030dc4d5a120519a752e59127
Sergans <gan7er@yandex.ru>
2022-08-08 10:27:28 +0300

    add models

---
+++ LibraryService/Models/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryService.Models
+{
+    public class Author
+    {   
+        public string Name { get; set; }
+        public string Lang { get; set; }
+    }
+}
